95cd1ad42a69ae1143d7dfb6c5cd4db5475292fb
VictorMunene <victor.munene@student.moringaschool.com>
2022-08-11 21:05:02 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Victor',
+    streetAddress: 'Runda'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject = {...employee};
+
+    newObject[key] = value;
+
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee};
+
+    delete newObject[key];
+
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+} 
